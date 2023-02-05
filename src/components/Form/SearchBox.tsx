64a5c82666af9a0a5d364fc4be87f76605ab9063
Antonio <netoifpe@gmail.com>
2023-02-05 17:30:05 -0300

    feat: create interface ModalCreateTasks

--- src/components/Form/SearchBox.tsx
+++ src/components/Form/SearchBox.tsx
@@ -1,56 +1,62 @@
-import { Button, Center, Flex } from "@chakra-ui/react";
+import { Button, Center, Flex, useDisclosure } from "@chakra-ui/react";
 import { FaSearch } from "react-icons/fa";
 import { theme } from "../../styles/theme";
+import { ModalCreateTask } from "../Modal/ModalCreateTask";
 import { Input } from "./input";
 
 interface SearchBoxProps {}
 
 export const SearchBox = ({}: SearchBoxProps) => {
+  const { isOpen, onClose, onOpen } = useDisclosure();
   return (
-    <Flex
-      mt="6"
-      paddingX={["4", "8"]}
-      paddingY="2"
-      paddingBottom="6"
-      borderBottomWidth="1px"
-      borderBottomColor="gray.50"
-      flexDirection={["column", "column", "row"]}
-    >
-      <Flex as="form">
-        <Input
-          placeholder="Pesquisar por tarefa"
-          w={["75vw", "75vw", "35vw"]}
-          name="Title"
-        />
-        <Center
-          as="button"
-          borderRadius="8px"
-          ml="2"
-          w="65px"
-          h="60px"
-          fontSize="2xl"
-          bg="purple.600"
-          _hover={{
-            bg: "purple.700",
-          }}
-        >
-          <FaSearch color={theme.colors.white} />
-        </Center>
-      </Flex>
-      <Button
-        bg="purple.500"
-        color="white"
-        paddingX="16"
-        mt={["4", "4", "0"]}
-        ml={["0px", "0px", "4"]}
-        h="60px"
-        borderRadius="8px"
-        _hover={{
-          bg: "purple.600",
-        }}
+    <>
+      <ModalCreateTask isOpen={isOpen} onClose={onClose} />
+      <Flex
+        mt="6"
+        paddingX={["4", "8"]}
+        paddingY="2"
+        paddingBottom="6"
+        borderBottomWidth="1px"
+        borderBottomColor="gray.50"
+        flexDirection={["column", "column", "row"]}
       >
-        Adicionar uma nova tarefa
-      </Button>
-    </Flex>
+        <Flex as="form">
+          <Input
+            placeholder="Pesquisar por tarefa"
+            w={["75vw", "75vw", "35vw"]}
+            name="Title"
+          />
+          <Center
+            as="button"
+            borderRadius="8px"
+            ml="2"
+            w="65px"
+            h="60px"
+            fontSize="2xl"
+            bg="purple.600"
+            _hover={{
+              bg: "purple.700",
+            }}
+          >
+            <FaSearch color={theme.colors.white} />
+          </Center>
+        </Flex>
+        <Button
+          bg="purple.500"
+          color="white"
+          paddingX="16"
+          mt={["4", "4", "0"]}
+          ml={["0px", "0px", "4"]}
+          h="60px"
+          borderRadius="8px"
+          _hover={{
+            bg: "purple.600",
+          }}
+          onClick={onOpen}
+        >
+          Adicionar uma nova tarefa
+        </Button>
+      </Flex>
+    </>
   );
 };
--- src/contexts/TasksContext.tsx
+++ src/contexts/TasksContext.tsx
@@ -1,3 +1,4 @@
+import { AxiosResponse } from "axios";
 import {
   createContext,
   ReactNode,
@@ -11,13 +12,47 @@ interface TasksContextProps {
   children: ReactNode;
 }
 
-interface TasksContextData {}
+interface Task {
+  id: string;
+  title: string;
+  description: string;
+  userId: string;
+  completed: boolean;
+}
+
+interface TasksContextData {
+  tasks: Task[];
+  createTask: (data: Omit<Task, "id">, accessToken: string) => Promise<void>;
+}
 
 export const TasksContext = createContext<TasksContextData>(
   {} as TasksContextData
 );
 
 export const TasksProvider = ({ children }: TasksContextProps) => {
-  const [tasks, setTasks] = useState([]);
-  return <TasksContext.Provider value={{}}>{children}</TasksContext.Provider>;
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  const createTask = useCallback(
+    async (data: Omit<Task, "id">, accessToken: string) => {
+      api
+        .post("/tasks", data, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        })
+        .then((response: AxiosResponse<Task>) => {
+          setTasks((oldTasks) => [...oldTasks, response.data]);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    []
+  );
+
+  return (
+    <TasksContext.Provider value={{ tasks, createTask }}>
+      {children}
+    </TasksContext.Provider>
+  );
 };
